e3165eb119cd07550af17aa838af6c4794b2d18f
Nata Nael <natanael280198@gmail.com>
2023-08-18 14:13:00 +0700

    add get-max and add numbers command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-const hello = (message: string) => {};
+import { Command } from "commander";
 
-console.log("wow!!!");
+const program = new Command();
+
+// declare the program
+
+program
+  .name("string-util")
+  .description("CLI to some Javascript string utilities")
+  .version("0.0.1");
+
+// add actions onto that CLI
+
+program
+  .argument("<string>", "string to log")
+  .action((message: string) => {
+    console.log(`Hello ${message}`);
+  })
+  .description("This action will say hello");
+
+program
+  .command("add <numbers...>")
+  .action((numbers: number[]) => {
+    const total = numbers.reduce((a, b) => a + b, 0);
+    console.log(`Total: ${total}`);
+  })
+  .description("add numbers and log the total");
+
+program
+  .command("get-max-number <numbers...>")
+  .action((numbers: number[]) => {
+    const max = Math.max(...numbers);
+    console.log(`Max: ${max}`);
+  })
+  .description("log the highest number in an array");
+
+// execute the CLI with the given arguments
+
+program.parse(process.argv);
